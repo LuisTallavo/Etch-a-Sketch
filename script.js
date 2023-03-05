0dfcfeb83ce627570be390ae83835a99df3be75e
Luis Tallavo <tallavol@mcmaster.ca>
2023-03-04 23:26:42 -0600

    Initial Commit
    Set up initial HTML/CSS/JS, created a 16x16 grid, and added a background colour

---
+++ script.js
@@ -0,0 +1,6 @@
+for (let i = 0; i < 160; i++) {
+    let gridSquare = document.createElement("div");
+    gridSquare.classList.add("gridSquare");
+
+    document.getElementById("sketchCont").append(gridSquare);
+}
